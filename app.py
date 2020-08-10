bc1a0d43edee77dbdcb6089d93a04c3c1df32044
rysharp <rysharprules@gmail.com>
2020-08-10 22:38:40 +0100

    Fetch to do items

--- app.py
+++ app.py
@@ -1,13 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
+import requests
+import sys
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
+BOARD_ID = "NSn3rIEE"
+BASE_URI = "https://api.trello.com/"
+QUERY = {
+   'key': '2caf651274e82aaad6db16b36a32631a',
+   'token': 'd3fa3f8b7f9706afed48b78175d1c76239df1425d5334ea4ff0e170b508be9e6'
+}
+
+def getData(path):
+    uri = BASE_URI + path
+    return requests.get(uri, params=QUERY).json()
+
 @app.route('/')
 def index():
-    return render_template('index.html', 
-        items=sorted(session.get_items(), key=lambda item: item['status'], reverse=True))
+    cards = getData(f'1/boards/{BOARD_ID}/cards')
+    lists = getData(f'1/boards/{BOARD_ID}/lists')
+    statuses = []
+    for list in lists:
+        status = {}
+        status['id'] = list['id']
+        status['title'] = list['name']
+        statuses.append(status)
+    items = []
+    for card in cards:
+        item = {}
+        item['id'] = card['id']
+        item['title'] = card['name']
+        item['status'] = [status['title'] for status in statuses if card['idList'] == status['id']][0]
+        items.append(item)
+    return render_template('index.html', items=items, statuses=statuses)
 
 @app.route('/create', methods=['POST'])
 def create():
