78e987119da2aff8644b1ab839390e84e7f477f1
Ryan Sharp <rysharprules@gmail.com>
2021-03-01 18:01:54 +0000

    Typo

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 docker login -u rysharp -p $DOCKERHUB_PW
 docker build --target prod -t rysharp/todo-app:${TRAVIS_COMMIT} .
 docker push rysharp/todo-app:${TRAVIS_COMMIT}
-docker tag rysharp/todo_app:${TRAVIS_COMMIT} registry.heroku.com/rysharp-todo-app/web
+docker tag rysharp/todo-app:${TRAVIS_COMMIT} registry.heroku.com/rysharp-todo-app/web
 docker push registry.heroku.com/rysharp-todo-app/web
 heroku container:release web -a rysharp-todo-app
