a3e14cd8d3a642dd21ac1ba91fc0165bfe49247b
rysharp <rysharprules@gmail.com>
2021-05-14 16:40:42 +0100

    FE altered so user's without writer role do not see options to update todo

--- todo_app/User.py
+++ todo_app/User.py
@@ -1,6 +1,14 @@
 from flask_login import UserMixin
 
 class User(UserMixin):
+
+    writer = "writer"
+    reader = "reader"
+
     def __init__(self, id):
         self.id = id
-        self.role = "writer" if id == "151856391" else "reader"
+        self.role = User.writer if id == "15185639" else User.reader
+
+    @property
+    def isWriter(self):
+        return self.role == User.writer
